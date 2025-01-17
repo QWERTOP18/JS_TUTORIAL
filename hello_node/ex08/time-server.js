a509cac8c416e3af126bf9915b533f9813364707
QWERTOP18 <mz18yui@gmail.com>
2025-01-18 01:48:06 +0900

    a

--- hello_node/ex08/time-server.js
+++ hello_node/ex08/time-server.js
@@ -1,25 +1,39 @@
-// nc localhost 8080
-// curl localhost:8080
-
 const net = require('net');
+
 if (process.argv.length !== 3) {
-    console.error('usage: node time-server.js port');
+    console.error('usage: node http-server.js port');
     process.exit(1);
 }
+
 const port = process.argv[2];
 
 const server = net.createServer((socket) => {
-  
-  const currentDate = new Date();
+    socket.on('data', (data) => {
+        const request = data.toString();
+        console.log(`Request received:\n${request}`);
+
+        const currentDate = new Date();
     
-  const formattedDate = currentDate.toISOString().slice(0, 16).replace('T', ' ');
+        const formattedDate = currentDate.toISOString().slice(0, 16).replace('T', ' ');
+        // create HTTP response
+        const body = `${formattedDate}\n`;
+        const response = 
+`HTTP/1.1 200 OK
+Content-Type: text/plain
+Content-Length: ${body.length}
 
-  //iso stands for International Organization for Standardization
+${body}`;
 
-  socket.write(formattedDate + '\n');
-  socket.end();
+        socket.write(response);
+        socket.end();
+      
+    });
+
+    socket.on('error', (err) => {
+        console.error(`Socket error: ${err.message}`);
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
